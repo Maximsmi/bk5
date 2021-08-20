52df798e1d0ea1356a5f23f745fbd525491ae76c
Maxim Smirnov <78700681+Maximsmi@users.noreply.github.com>
2021-08-20 09:50:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import telebot   # библитека для работы с telegramm
 from telebot import types
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['TELEGRAM_TOKEN'])
 
 # Token для телеграмм бота
-token = "TELEGRAM_TOKEN"
+token = 'TELEGRAM_TOKEN'
 bot = telebot.TeleBot(token)
 
 # Обрабатываем команду /start
