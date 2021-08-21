f388263119df2dccd019ee41c63e6c87469f3207
Maxim Smirnov <78700681+Maximsmi@users.noreply.github.com>
2021-08-21 08:45:23 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
 import telebot   # библитека для работы с telegramm
-from telebot import types
 import os
+from boto.telegram.connection import telegramConnection
 
-# import boto
-# from boto.s3.connection import S3Connection
-# s3 = S3Connection(os.environ['TELEGRAM_TOKEN'])
+token = telegramConnection(os.environ['TELEGRAM_TOKEN'])
 
 # Token для телеграмм бота
-token = 'TELEGRAM_TOKEN'
-print ("token: ", token)
-bot = telebot.TeleBot('TELEGRAM_TOKEN')
+# token = 'TELEGRAM_TOKEN'
+bot = telebot.TeleBot(token)
+
 
 # Обрабатываем команду /start
 @bot.message_handler(commands=['start'])
-def start_message(message):
-  bot.send_message(message.chat.id, 'Меня зовут бот. Чем могу вам помочь?')
+def ferst_message(message):
+    bot.send_message(message.chat.id, "Привет!")
 
 
 bot.polling(none_stop=True)
