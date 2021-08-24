65a53a7d474d33f29489acaf58b76bb23f2a76d4
Maxim Smirnov <78700681+Maximsmi@users.noreply.github.com>
2021-08-24 16:34:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ import os
 
 # Token для телеграмм бота
 # token = 'TELEGRAM_TOKEN'
-bot = telebot.TeleBot('t_token_bk5')
-
+#bot = telebot.TeleBot('t_token_bk5')
+print ('t_token_bk5')
 
 # Обрабатываем команду /start
-@bot.message_handler(commands=['start'])
-def ferst_message(message):
-    bot.send_message(message.chat.id, "Привет!")
+#@bot.message_handler(commands=['start'])
+#def ferst_message(message):
+#    bot.send_message(message.chat.id, "Привет!")
 
 
-bot.polling(none_stop=True)
+#bot.polling(none_stop=True)
