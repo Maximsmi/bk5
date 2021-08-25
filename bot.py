c7b0df90cd75c88c0b8a3316eed9979fe2bb7480
Maxim Smirnov <78700681+Maximsmi@users.noreply.github.com>
2021-08-25 08:13:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 # Token для телеграмм бота
 # token = 'TELEGRAM_TOKEN'
 #bot = telebot.TeleBot('t_token_bk5')
-print (t_token_bk5)
+print (os.environ.get('TELEGRAM_TOKEN', none))
 
 # Обрабатываем команду /start
 #@bot.message_handler(commands=['start'])
