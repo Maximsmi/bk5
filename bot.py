74fad2ffb39ca1424efe5fb927f43e8d601304ea
Maxim Smirnov <78700681+Maximsmi@users.noreply.github.com>
2021-08-25 08:27:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import telebot   # библитека для работы с telegramm
 import os
-import python-dotenv
 
 from os import environ
 from dotenv import load_dotenv()
 
 load_dotenv()
+environ.get('t_token_bk5')
+
+from config import (t_token_bk5)
 # Token для телеграмм бота
 # token = 'TELEGRAM_TOKEN'
 #bot = telebot.TeleBot('t_token_bk5')
-environ.get('t_token_bk5')
+print(t_token_bk5)
 
 # Обрабатываем команду /start
 #@bot.message_handler(commands=['start'])
